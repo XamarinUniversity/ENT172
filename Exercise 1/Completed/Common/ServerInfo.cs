db2f225bf200371f190b16aa37ec88bd99ec2662
Adam Patridge <patridge@gmail.com>
2017-10-06 10:35:47 -0600

    Update NuGet packages and latest exercises.

---
+++ Exercise 1/Completed/Common/ServerInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ComicBook
+{
+	public static class ServerInfo
+	{
+		public static Uri AuthorizationEndpoint = new Uri("http://ent172-auth.azurewebsites.net/oauth/authorize");
+		public static Uri TokenEndpoint         = new Uri("http://ent172-auth.azurewebsites.net/oauth/token");
+		public static Uri ApiEndpoint           = new Uri("http://ent172-auth.azurewebsites.net/api/whoami");
+		public static Uri RedirectionEndpoint 		= new Uri("http://localhost/");
+		public static string ClientId 				= "A8375B66";
+		public static string ClientSecret 			= "A32D8C3CBE9A";
+	}
+}
